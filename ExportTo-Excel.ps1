253cf4e08e09005bf1f837020d05d664e19ad50f
CarlyWarly <carl@email.com>
2020-04-22 20:29:57 +0100

    Update ExportTo-Excel.ps1

--- ExportTo-Excel.ps1
+++ ExportTo-Excel.ps1
@@ -37,6 +37,7 @@ Function ExportTo-Excel {
                 If ($PSObject -ne $null) {
                     $ret = @()
                     Foreach ($line in $PSObject) {
+                        $NewObject = New-Object -TypeName PSObject
                         foreach ($header in $line.psobject.properties) {
                             $Value = $header.value
                             if ($Value -ne $null) {
